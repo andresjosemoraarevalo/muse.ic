e8b42c9f733903a4ef80cce9aa0939a028efdb13
Diego Alejandro Gomez Parra <diko2140@gmail.com>
2020-09-25 12:10:38 -0500

    Ident

--- museic-client/src/App.js
+++ museic-client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 //components
 import Navbar from "./components/Navbar";
 //Pages
@@ -13,36 +13,36 @@ import signup from "./pages/signup";
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: '#990099',
-      main: '#800080',
-      dark: '#250025',
-      contrastText: '#fff'
+      light: "#990099",
+      main: "#800080",
+      dark: "#250025",
+      contrastText: "#fff",
     },
     secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
-    }
-  }
-})
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+      contrastText: "#fff",
+    },
+  },
+});
 
 function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <div className="App">
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home} />
-              <Route exact path="/login" component={login} />
-              <Route exact path="/signup" component={signup} />
-            </Switch>
-          </div>
-        </Router>
-    </div>
-    </MuiThemeProvider>
+	return (
+		<MuiThemeProvider theme={theme}>
+			<div className="App">
+        		<Router>
+					<Navbar />
+					<div className="container">
+						<Switch>
+							<Route exact path="/" component={home} />
+							<Route exact path="/login" component={login} />
+							<Route exact path="/signup" component={signup} />
+						</Switch>
+					</div>
+				</Router>
+      		</div>
+    	</MuiThemeProvider>
   );
 }
 

d8eb2d5dc649084f7a9219d9b0a56d8625a809f3
Diego Alejandro Gomez Parra <diko2140@gmail.com>
2020-09-25 12:03:14 -0500

    React project
    Se añadió el proyecto de react para trabajar la GUI

---
+++ museic-client/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+//components
+import Navbar from "./components/Navbar";
+//Pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#990099',
+      main: '#800080',
+      dark: '#250025',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  }
+})
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+    </div>
+    </MuiThemeProvider>
+  );
+}
+
+export default App;

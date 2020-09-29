296ffe6e99b2f35b45ab7fcc83f81e4b12631500
Diego Alejandro Gomez Parra <diko2140@gmail.com>
2020-09-28 22:54:59 -0500

    Log in page
    ya casi esta la interfaz de log in

--- museic-client/src/pages/login.js
+++ museic-client/src/pages/login.js
@@ -1,13 +1,135 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import imagenLogin from '../images/loginimage.jpg';
+
+//MUI stuff
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Avatar from '@material-ui/core/Avatar';
+import PersonIcon from '@material-ui/icons/Person';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+    root: {
+        /*textAlign: 'center',*/
+        height: '100vh'
+    },
+    image: {
+        backgroundImage: "url(" + imagenLogin + ")",
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center'
+    },
+    paper: {
+        margin: '64px 32px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    form: {
+        width: '100%',
+        marginTop: '8px'
+    },
+    avatar: {
+        margin: '8px',
+        backgroundColor: "#900090"
+    }
+}
+
+
 
 class login extends Component {
+    constructor(){
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            errors: {}
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
     render() {
+        const  { classes } = this.props;
         return (
-            <div>
-                <h1>Pagina de login</h1>
-            </div>
+            <Grid container className={classes.root}>
+                <CssBaseline />
+                <Grid item xs={false} sm={4} md={7} className={classes.image}/>
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <div className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <PersonIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Iniciar Sesión
+                        </Typography>
+                        <form className={classes.form} noValidate onSubmit={this.handleSubmit}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Correo"
+                            name="email"
+                            type="email"
+                            autoComplete="email"
+                            autoFocus
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            />
+                            <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            type="password"
+                            label="Contraseña"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            />
+                            <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Recordarme"
+                            />
+                            <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            >
+                            Ingresar
+                            </Button>
+                        </form>
+                    </div>
+                </Grid>
+            </Grid>
         )
     }
 }
 
-export default login
+login.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(login);

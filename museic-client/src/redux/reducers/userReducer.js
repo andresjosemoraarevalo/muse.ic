e90e0cbfd29a0a49fc5ea23c402da8a2ec771167
Camilo Hoyos <camilohoyos@javeriana.edu.co>
2020-10-12 21:18:08 -0500

    Se agrega el estado de carga en caso de que sea verdadero

--- museic-client/src/redux/reducers/userReducer.js
+++ museic-client/src/redux/reducers/userReducer.js
@@ -1,8 +1,9 @@
 
-import { SET_USER, SET_AUTHENTICATED, SET_UNAUTHENTICATED } from '../types';
+import { SET_USER, SET_AUTHENTICATED, SET_UNAUTHENTICATED, LOADING_USER } from '../types';
 
 const initialState = {
     authenticated: false,
+    loading: false,
     credentials: {},
     seguidos: []
 };
@@ -19,8 +20,15 @@ export default function(state = initialState, action){
         case SET_USER:
             return {
                 authenticated: true,
+                loading: false,
                 ...action.payload
             };
+        case LOADING_USER:
+            return{
+                ...state,
+                loading: true
+            }
+
         default:
             return state;
     }
